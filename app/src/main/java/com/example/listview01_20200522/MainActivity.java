cf3c3e6ccd71e0ee039030ce6f980e7e8c0464dc
moonheuiwon <wonheuimoon@gmail.com>
2020-05-22 11:34:33 +0900

    수강생 담는 멤버변수 리스트 작성

--- app/src/main/java/com/example/listview01_20200522/MainActivity.java
+++ app/src/main/java/com/example/listview01_20200522/MainActivity.java
@@ -28,10 +28,16 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+
     }
 
     @Override
     public void setValues() {
+
+        addStudents();
+    }
+
+    void  addStudents() {
 
         ourClassStudentList.add(new Student("조경진", "서울시 은평구"));
         ourClassStudentList.add(new Student("강희진", "인천시 남동구"));
